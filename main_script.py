73c7b300a7941fff543cedd7af936b740a1f148f
aziznal <aziznal.dev@gmail.com>
2020-08-20 21:24:03 +0300

    Prepare script to run from 9 to 5

--- main_script.py
+++ main_script.py
@@ -19,10 +19,10 @@ def run_script():
 
     global spyder
 
-    loop_count = 10
-    interval = 2    # seconds
+    loop_count = 128
+    interval = create_new_loop_interval(9, 17, loop_count)
 
-    print(f"Each interval will take {interval} seconds")
+    print(f"New data will be collected every {interval} seconds")
 
     current_loop = 0
 
